48830e8c6f780f073242aab2cc5ee59108ebd483
bruno-pequeno <bru.pequeno@gmail.com>
2024-03-28 00:58:09 -0300

    feat: Initializing unit tests of PlanetService

---
+++ src/test/java/udemy/testesautomatizados/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package udemy.testesautomatizados.common;
+
+import udemy.testesautomatizados.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("Coruscant", "city planet", "urban sprawl");
+}
---
+++ src/test/java/udemy/testesautomatizados/domain/PlanetServiceTest.java
@@ -0,0 +1,21 @@
+package udemy.testesautomatizados.domain;
+
+import static udemy.testesautomatizados.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PlanetService.class)
+public class PlanetServiceTest {
+    @Autowired
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet(){
+        Planet sut = planetService.create(PLANET);
+
+        assertThat(sut).isEqualTo(PLANET);
+    }
+}
