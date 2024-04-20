561d44fea5147d2364cae3d0b993fdb1a0fc4694
bruno-pequeno <bru.pequeno@gmail.com>
2024-04-20 16:22:24 -0300

    feat: Adding dinamic querys

--- src/main/java/udemy/testesautomatizados/domain/PlanetRepository.java
+++ src/main/java/udemy/testesautomatizados/domain/PlanetRepository.java
@@ -1,9 +1,14 @@
 package udemy.testesautomatizados.domain;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PlanetRepository extends CrudRepository<Planet, Long> {
+public interface PlanetRepository extends CrudRepository<Planet, Long>, QueryByExampleExecutor<Planet> {
+    @Override
+    <S extends Planet> List<S> findAll(Example<S> example);
     Optional<Planet> findByName(String name);
 }
